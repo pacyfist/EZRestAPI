23d8c296b9b95fa8d782b1f8ca3777523ea53168
Filip Franik <pacyfist@gmail.com>
2025-01-27 23:38:41 +0100

    Dropped scriban dependency from DbContextGenerator.cs

--- EZRestAPI/Generators/DbContextGenerator.cs
+++ EZRestAPI/Generators/DbContextGenerator.cs
@@ -1,46 +1,50 @@
 ﻿namespace EZRestAPI.Generators;
 
-using Scriban;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 using EZRestAPI.Providers;
+using System.CodeDom.Compiler;
 
 [Generator(LanguageNames.CSharp)]
 public class DbContextGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var template = Template.Parse("""
-            namespace {{AssemblyName}};
-
-            using Microsoft.EntityFrameworkCore;
-            
-            public partial class CustomDbContext : DbContext
-            {
-                public CustomDbContext(DbContextOptions<CustomDbContext> options)
-                    : base(options)
-                {
-
-                }
-
-                {{~ for model in models ~}}
-                public DbSet<{{model.ClassName}}> {{model.PluralName}} { get; set; } = null!;
-                {{~ end ~}}
-            }
-            """);
-
         var modelsProvider = context.SyntaxProvider.GetModels().Collect();
 
         context.RegisterSourceOutput(modelsProvider, (ctx, models) =>
         {
+            var writer = new IndentedTextWriter(new StringWriter());
+
+            writer.WriteLine($"namespace {models.First().AssemblyName};");
+            writer.WriteLine();
+            writer.WriteLine("using Microsoft.EntityFrameworkCore;");
+            writer.WriteLine();
+            writer.WriteLine("public partial class CustomDbContext : DbContext");
+            writer.WriteLine("{");
+            writer.Indent++;
+            writer.WriteLine("public CustomDbContext(DbContextOptions<CustomDbContext> options)");
+            writer.Indent++;
+            writer.WriteLine(": base(options)");
+            writer.Indent--;
+            writer.WriteLine("{");
+            writer.WriteLine("}");
+            writer.WriteLine();
+
+            foreach (var model in models)
+            {
+                writer.WriteLine($"public DbSet<{model.ClassName}> {model.PluralName} {{ get; set; }} = null!;");
+            }
+
+            writer.Indent--;
+            writer.WriteLine("}");
+
             ctx.AddSource(
                 "CustomDbContext.g.cs",
-                SourceText.From(template.Render(new
-                {
-                    models.First().AssemblyName,
-                    models
-                }, memberRenamer: n => n.Name), Encoding.UTF8));
+                SourceText.From(
+                    writer.InnerWriter.ToString(),
+                    Encoding.UTF8));
         });
     }
 }
